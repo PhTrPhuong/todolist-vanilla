194969455b1ae88cc64d6272fed32e50b4a21b00
TrPhuong <trphuong1909@gmail.com>
2023-11-30 18:07:45 +0700

    init todo list

---
+++ script.js
@@ -0,0 +1,154 @@
+let todos = [];
+
+/*---------*/
+/*--- HANDLE ADD FORM ---*/
+const addInputNode = document.getElementById("addInput");
+const addFormNode = document.getElementById("addForm");
+
+addFormNode.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (addInputNode.value) {
+        addTodo(addInputNode.value);
+        addInputNode.value = "";
+    }
+});
+
+function addTodo(addedValue) {
+    const newTodo = {
+        id: Date.now(),
+        label: addedValue,
+        isDone: false,
+    };
+    todos.unshift(newTodo);
+
+    renderTodos();
+}
+
+/*---------*/
+/*--- HANDLE RENDER TODO ITEMS ---*/
+function renderTodos() {
+    // create todo list node
+    const todoListNode = document.getElementById("todoList");
+    todoListNode.innerHTML = "";
+
+    // loop through "todos" array to handle render todo-items
+    todos.forEach((todo) => {
+        // this is todo-item's keys
+        const { id, label, isDone, isEditting } = todo || {};
+
+        /* --- */
+        // todoItemNode: wrap toto-item content
+        const todoItemNode = document.createElement("li");
+        todoItemNode.className = `todo-item ${isDone ? "done" : ""}`;
+        todoItemNode.id = id;
+
+        // labelNode: render todo-item label
+        const labelNode = document.createElement("span");
+        labelNode.className = "todo-label";
+        labelNode.innerText = label;
+
+        // actionNode: wrap todo-item actions
+        const actionNode = document.createElement("div");
+        actionNode.className = "todo-action";
+
+        // deleteBtnNote: button handle delete action
+        const deleteBtnNode = document.createElement("button");
+        deleteBtnNode.className = "btn btn-delete";
+        deleteBtnNode.innerText = "Delete";
+        deleteBtnNode.addEventListener("click", (event) => {
+            event.preventDefault();
+            deleteTodo(id);
+        });
+
+        // editBtnNote: button handle edit action
+        const editBtnNode = document.createElement("button");
+        editBtnNode.className = "btn btn-edit";
+        editBtnNode.innerText = "Edit";
+        editBtnNode.addEventListener("click", (event) => {
+            event.preventDefault();
+            toggleEditView(id);
+        });
+
+        // doneBtnNode: button handle done action
+        const doneBtnNode = document.createElement("button");
+        doneBtnNode.classList = "btn btn-done";
+        doneBtnNode.innerText = isDone ? "Undone" : "Done";
+        doneBtnNode.addEventListener("click", (event) => {
+            event.preventDefault();
+            updateTodoStatus(id);
+        });
+
+        /* --- */
+        // editInputNode: input handle get user edited-label
+        const editInputNode = document.createElement("input");
+        editInputNode.className = "input editInput";
+        editInputNode.value = label;
+
+        // saveBtnNode: button handle save user edited-label
+        const saveBtnNode = document.createElement("button");
+        saveBtnNode.className = "btn";
+        saveBtnNode.innerText = "Save";
+
+        // editFromNode: form cover & handle submit edited-label
+        const editFormNode = document.createElement("form");
+        editFormNode.className = "form editForm";
+        editFormNode.addEventListener("submit", (event) => {
+            event.preventDefault();
+            if (editInputNode.value) {
+                updateTodoLabel(id, editInputNode.value);
+                toggleEditView(id);
+                editInputNode.value = "";
+            }
+        });
+
+        /* --- */
+        // if "isEditting" true, render edit view with editFormNode
+        if (isEditting) {
+            editFormNode.appendChild(editInputNode);
+            editFormNode.appendChild(saveBtnNode);
+            todoItemNode.appendChild(editFormNode);
+        }
+        // if "isEditting" false, render info view with labelNode & actionNode
+        else {
+            actionNode.appendChild(deleteBtnNode);
+            !isDone && actionNode.appendChild(editBtnNode);
+            actionNode.appendChild(doneBtnNode);
+            todoItemNode.appendChild(labelNode);
+            todoItemNode.appendChild(actionNode);
+        }
+
+        // add this todoItemNode into todoListNode
+        todoListNode.appendChild(todoItemNode);
+    });
+}
+
+/*---------*/
+/*--- FUNCTIONS - handle delete todo item by id ---*/
+function deleteTodo(id) {
+    todos = todos.filter((todo) => todo.id !== id);
+    renderTodos();
+}
+
+/*--- FUNCTIONS - handle toggle todo item's edit view ---*/
+function toggleEditView(id) {
+    todos = todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditting: !todo.isEditting } : todo
+    );
+    renderTodos();
+}
+
+/*--- FUNCTIONS - handle update todo item's label ---*/
+function updateTodoLabel(id, editedLabel) {
+    todos = todos.map((todo) =>
+        todo.id === id ? { ...todo, label: editedLabel } : todo
+    );
+    renderTodos();
+}
+
+/*--- FUNCTIONS - handle update todo item's status ---*/
+function updateTodoStatus(id) {
+    todos = todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+    );
+    renderTodos();
+}
